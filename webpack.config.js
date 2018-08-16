b8468faffd22746031efe8e875d90a988cd1e421
xxj95719 <87416959@qq.com>
2018-08-16 19:21:27 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,39 @@
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+const nodeExternals = require('webpack-node-externals') //解绑模块
+const path = require('path')
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: {
-    index: './src/index.js',
-    vendor: ['downloadjs']
+    index: './src/index.js'
   },
   output: {
-    filename: '[name].js',
-    path: __dirname + '/dist'
+    filename: 'index.js',
+    path: __dirname + '/dist',
+    library: 'customJson2excel',
+    libraryTarget: 'commonjs2'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  externals: [
+    // ...Object.keys(packageJson.dependencies),
+    nodeExternals({
+      modulesFromFile: true
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        include: [path.resolve(__dirname, './src')]
+      }
+    ]
   },
   plugins: [
     new CleanWebpackPlugin(['dist'], {
       verbose: false
     })
-  ]
+  ],
+  devtool: 'source-map'
 }

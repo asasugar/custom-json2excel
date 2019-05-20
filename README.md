4b6ddb56e84b27492ef11f3edb719717a3dfedbf
xxj95719 <xxj95719@gmail.com>
2019-05-20 17:28:18 +0800

    :sparkles: 新增`生成开始`与`生成成功`的cb函数

--- README.md
+++ README.md
@@ -13,39 +13,39 @@ npm install custom-json2excel
 1、直接转化 json：
 
 ```js
-import Json2excel from 'custom-json2excel'
+import Json2excel from "custom-json2excel";
 const data = [
   {
-    name: '哈哈',
+    name: "哈哈",
     age: 1,
-    sex: '男',
-    companyName: '公司1',
-    companyAddress: '公司地址1'
+    sex: "男",
+    companyName: "公司1",
+    companyAddress: "公司地址1"
   },
   {
-    name: '呵呵',
+    name: "呵呵",
     age: 2,
-    sex: '女',
-    companyName: '公司2',
-    companyAddress: '公司地址2'
+    sex: "女",
+    companyName: "公司2",
+    companyAddress: "公司地址2"
   },
   {
-    name: '嘻嘻',
+    name: "嘻嘻",
     age: 3,
-    sex: '男',
-    companyName: '公司3',
-    companyAddress: '公司地址3'
+    sex: "男",
+    companyName: "公司3",
+    companyAddress: "公司地址3"
   },
   {
-    name: '啦啦',
+    name: "啦啦",
     age: 4,
-    sex: '女',
-    companyName: '公司4',
-    companyAddress: '公司地址4'
+    sex: "女",
+    companyName: "公司4",
+    companyAddress: "公司地址4"
   }
-]
-const json2excel = new Json2excel({ data })
-json2excel.generate()
+];
+const json2excel = new Json2excel({ data });
+json2excel.generate();
 ```
 
 ![image](https://github.com/xxj95719/custom-json2excel/blob/master/images/noKepMap.png)
@@ -53,46 +53,46 @@ json2excel.generate()
 2、自定义头部无需过滤字段时的使用方式：
 
 ```js
-import Json2excel from 'custom-json2excel'
+import Json2excel from "custom-json2excel";
 const data = [
   {
-    name: '哈哈',
+    name: "哈哈",
     age: 1,
-    sex: '男',
-    companyName: '公司1',
-    companyAddress: '公司地址1'
+    sex: "男",
+    companyName: "公司1",
+    companyAddress: "公司地址1"
   },
   {
-    name: '呵呵',
+    name: "呵呵",
     age: 2,
-    sex: '女',
-    companyName: '公司2',
-    companyAddress: '公司地址2'
+    sex: "女",
+    companyName: "公司2",
+    companyAddress: "公司地址2"
   },
   {
-    name: '嘻嘻',
+    name: "嘻嘻",
     age: 3,
-    sex: '男',
-    companyName: '公司3',
-    companyAddress: '公司地址3'
+    sex: "男",
+    companyName: "公司3",
+    companyAddress: "公司地址3"
   },
   {
-    name: '啦啦',
+    name: "啦啦",
     age: 4,
-    sex: '女',
-    companyName: '公司4',
-    companyAddress: '公司地址4'
+    sex: "女",
+    companyName: "公司4",
+    companyAddress: "公司地址4"
   }
-]
+];
 const keyMap = {
-  name: '姓名',
-  age: '年龄',
-  sex: '性别',
-  companyName: '公司名称',
-  companyAddress: '公司地址'
-}
-const json2excel = new Json2excel({ data, keyMap })
-json2excel.generate()
+  name: "姓名",
+  age: "年龄",
+  sex: "性别",
+  companyName: "公司名称",
+  companyAddress: "公司地址"
+};
+const json2excel = new Json2excel({ data, keyMap });
+json2excel.generate();
 ```
 
 ![image](https://github.com/xxj95719/custom-json2excel/blob/master/images/noTitle.png)
@@ -100,47 +100,47 @@ json2excel.generate()
 3、需要过滤字段时的使用方式：
 
 ```js
-import Json2excel from 'custom-json2excel'
+import Json2excel from "custom-json2excel";
 const data = [
   {
-    name: '哈哈',
+    name: "哈哈",
     age: 1,
-    sex: '男',
-    companyName: '公司1',
-    companyAddress: '公司地址1'
+    sex: "男",
+    companyName: "公司1",
+    companyAddress: "公司地址1"
   },
   {
-    name: '呵呵',
+    name: "呵呵",
     age: 2,
-    sex: '女',
-    companyName: '公司2',
-    companyAddress: '公司地址2'
+    sex: "女",
+    companyName: "公司2",
+    companyAddress: "公司地址2"
   },
   {
-    name: '嘻嘻',
+    name: "嘻嘻",
     age: 3,
-    sex: '男',
-    companyName: '公司3',
-    companyAddress: '公司地址3'
+    sex: "男",
+    companyName: "公司3",
+    companyAddress: "公司地址3"
   },
   {
-    name: '啦啦',
+    name: "啦啦",
     age: 4,
-    sex: '女',
-    companyName: '公司4',
-    companyAddress: '公司地址4'
+    sex: "女",
+    companyName: "公司4",
+    companyAddress: "公司地址4"
   }
-]
+];
 const keyMap = {
-  name: '姓名',
-  age: '年龄',
-  sex: '性别',
-  companyName: '公司名称',
-  companyAddress: '公司地址'
-}
-const filters = ['sex']
-const json2excel = new Json2excel({ data, keyMap, filters })
-json2excel.generate()
+  name: "姓名",
+  age: "年龄",
+  sex: "性别",
+  companyName: "公司名称",
+  companyAddress: "公司地址"
+};
+const filters = ["sex"];
+const json2excel = new Json2excel({ data, keyMap, filters });
+json2excel.generate();
 ```
 
 ![image](https://github.com/xxj95719/custom-json2excel/blob/master/images/filter.png)
@@ -148,63 +148,126 @@ json2excel.generate()
 4、需要表格标题时的使用方式：
 
 ```js
-import Json2excel from 'custom-json2excel'
+import Json2excel from "custom-json2excel";
 const data = [
   {
-    name: '哈哈',
+    name: "哈哈",
     age: 1,
-    sex: '男',
-    companyName: '公司1',
-    companyAddress: '公司地址1'
+    sex: "男",
+    companyName: "公司1",
+    companyAddress: "公司地址1"
   },
   {
-    name: '呵呵',
+    name: "呵呵",
     age: 2,
-    sex: '女',
-    companyName: '公司2',
-    companyAddress: '公司地址2'
+    sex: "女",
+    companyName: "公司2",
+    companyAddress: "公司地址2"
   },
   {
-    name: '嘻嘻',
+    name: "嘻嘻",
     age: 3,
-    sex: '男',
-    companyName: '公司3',
-    companyAddress: '公司地址3'
+    sex: "男",
+    companyName: "公司3",
+    companyAddress: "公司地址3"
   },
   {
-    name: '啦啦',
+    name: "啦啦",
     age: 4,
-    sex: '女',
-    companyName: '公司4',
-    companyAddress: '公司地址4'
+    sex: "女",
+    companyName: "公司4",
+    companyAddress: "公司地址4"
   }
-]
+];
 const keyMap = {
-  name: '姓名',
-  age: '年龄',
-  sex: '性别',
-  companyName: '公司名称',
-  companyAddress: '公司地址'
-}
-const filters = ['sex']
+  name: "姓名",
+  age: "年龄",
+  sex: "性别",
+  companyName: "公司名称",
+  companyAddress: "公司地址"
+};
+const filters = ["sex"];
 const title = [
-  { name: '个人信息', colspan: 3 },
-  { name: '公司信息', colspan: 2 }
-]
-const json2excel = new Json2excel({ data, title })
-json2excel.generate()
+  { name: "个人信息", colspan: 3 },
+  { name: "公司信息", colspan: 2 }
+];
+const json2excel = new Json2excel({ data, keyMap, filters, title });
+json2excel.generate();
+```
+
+5、绑定回调函数的使用方式：
+
+```js
+import Json2excel from "custom-json2excel";
+const data = [
+  {
+    name: "哈哈",
+    age: 1,
+    sex: "男",
+    companyName: "公司1",
+    companyAddress: "公司地址1"
+  },
+  {
+    name: "呵呵",
+    age: 2,
+    sex: "女",
+    companyName: "公司2",
+    companyAddress: "公司地址2"
+  },
+  {
+    name: "嘻嘻",
+    age: 3,
+    sex: "男",
+    companyName: "公司3",
+    companyAddress: "公司地址3"
+  },
+  {
+    name: "啦啦",
+    age: 4,
+    sex: "女",
+    companyName: "公司4",
+    companyAddress: "公司地址4"
+  }
+];
+const keyMap = {
+  name: "姓名",
+  age: "年龄",
+  sex: "性别",
+  companyName: "公司名称",
+  companyAddress: "公司地址"
+};
+const filters = ["sex"];
+const title = [
+  { name: "个人信息", colspan: 3 },
+  { name: "公司信息", colspan: 2 }
+];
+const json2excel = new Json2excel({
+  data,
+  keyMap,
+  filters,
+  title,
+  onStart: () => {
+    console.log("开始");
+  },
+  onSuccess: () => {
+    console.log("成功");
+  }
+});
+json2excel.generate();
 ```
 
 ![image](https://github.com/xxj95719/custom-json2excel/blob/master/images/title.png)
 
 ## Props type
 
-| _Prop_  | _Type_ | _Defaults_ | _Required_ | _Description_                                                      |
-| :------ | :----- | :--------- | :--------- | ------------------------------------------------------------------ |
-| data    | Array  | []         | ✓          | 转化成表格初始 json 数据                                           |
-| filters | Array  | []         | ×          | 需要过滤的字段数组                                                 |
-| footer  | Array  | []         | ×          | 表格最后一列名称，参数同 title                                     |
-| keyMap  | Object | {}         | ×          | keyMap 映射表，用于自定义表格头部名称                              |
-| name    | String | excel      | ×          | excel 表格名称                                                     |
-| title   | Array  | []         | ×          | 表格标题名称 {name:String,colspan:Number} name:名称， colspan:列数 |
-| type    | String | xls        | ×          | 生成的表格类型，可选值(xls、csv)                                   |
+| _Prop_    | _Type_   | _Defaults_ | _Required_ | _Description_                                                      |
+| :-------- | :------- | :--------- | :--------- | ------------------------------------------------------------------ |
+| data      | Array    | []         | ✓          | 转化成表格初始 json 数据                                           |
+| filters   | Array    | []         | ×          | 需要过滤的字段数组                                                 |
+| footer    | Array    | []         | ×          | 表格最后一列名称，参数同 title                                     |
+| keyMap    | Object   | {}         | ×          | keyMap 映射表，用于自定义表格头部名称                              |
+| name      | String   | excel      | ×          | excel 表格名称                                                     |
+| title     | Array    | []         | ×          | 表格标题名称 {name:String,colspan:Number} name:名称， colspan:列数 |
+| type      | String   | xls        | ×          | 生成的表格类型，可选值(xls、csv)                                   |
+| onStart   | Function |            | ×          | 生成 Excel 前的回调函数                                            |
+| onSuccess | Function |            | ×          | 生成 Excel 成功的回调函数                                          |
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import download from 'downloadjs'
+import download from "downloadjs";
 export default class Json2Excel {
   constructor({
     data = [],
@@ -7,78 +7,88 @@ export default class Json2Excel {
     filters = [],
     footer = [],
     keyMap = {},
-    name = 'excel',
+    name = "excel",
     title = [],
-    type = 'xls'
+    type = "xls",
+    onStart = () => {},
+    onSuccess = () => {}
   }) {
-    this.data = data
-    this.exportFields = exportFields
-    this.fields = fields
-    this.filters = filters
-    this.footer = footer
-    this.keyMap = keyMap
-    this.name = name
-    this.title = title
-    this.type = type
+    this.data = data;
+    this.exportFields = exportFields;
+    this.fields = fields;
+    this.filters = filters;
+    this.footer = footer;
+    this.keyMap = keyMap;
+    this.name = name;
+    this.title = title;
+    (this.type = type), (this.onStart = onStart);
+    this.onSuccess = onSuccess;
   }
   downloadFields() {
-    if (this.fields !== undefined) return this.fields
+    if (this.fields !== undefined) return this.fields;
 
-    if (this.exportFields !== undefined) return this.exportFields
+    if (this.exportFields !== undefined) return this.exportFields;
   }
   toChsKeys(json, keyMap) {
     return json.map(item => {
       if (this.filters.length === 0) {
         for (let key in item) {
           if (keyMap.hasOwnProperty(key)) {
-            item[keyMap[key]] = item[key]
-            delete item[key]
+            item[keyMap[key]] = item[key];
+            delete item[key];
           }
         }
       } else {
         for (let filterItem of this.filters) {
-          delete item[filterItem]
+          delete item[filterItem];
           for (let key in item) {
             if (keyMap.hasOwnProperty(key)) {
-              item[keyMap[key]] = item[key]
-              delete item[key]
+              item[keyMap[key]] = item[key];
+              delete item[key];
             }
           }
         }
       }
-      return item
-    })
+      return item;
+    });
   }
   generate() {
     if (!this.data.length) {
-      return
+      return;
     }
-    let json = this.getProcessedJson(this.data, this.downloadFields())
+    this.onStart();
+    let json = this.getProcessedJson(this.data, this.downloadFields());
     if (
       Object.keys(this.keyMap).length !== 0 &&
       this.keyMap instanceof Object
     ) {
-      json = this.toChsKeys(json, this.keyMap)
+      json = this.toChsKeys(json, this.keyMap);
     }
-    if (this.type == 'csv') {
+    if (this.type == "csv") {
       return this.export(
         this.jsonToCSV(json),
         `${this.name}.${this.type}`,
-        'application/csv'
-      )
+        "application/csv"
+      );
     }
     return this.export(
       this.jsonToXLS(json),
       `${this.name}.${this.type}`,
-      'application/vnd.ms-excel'
-    )
+      "application/vnd.ms-excel"
+    );
   }
   /*
   使用 downloadjs 生成下载链接
   */
-  export (data, filename, mime) {
-    let blob = this.base64ToBlob(data, mime)
-    download(blob, filename, mime)
+  export(data, filename, mime) {
+    new Promise((resolve, reject) => {
+      let blob = this.base64ToBlob(data, mime);
+      resolve(download(blob, filename, mime));
+    })
+      .then(res => {
+        this.onSuccess();
+      })
+      .catch(err => {});
   }
   /*
   jsonToXLS
@@ -87,38 +97,38 @@ export default class Json2Excel {
   */
   jsonToXLS(data) {
     let xlsTemp =
-      '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><meta name=ProgId content=Excel.Sheet> <meta name=Generator content="Microsoft Excel 11"><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>${table}</table></body></html>'
-    let xlsData = '<thead><tr>'
+      '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><meta name=ProgId content=Excel.Sheet> <meta name=Generator content="Microsoft Excel 11"><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>${table}</table></body></html>';
+    let xlsData = "<thead><tr>";
     //Header
     if (this.title.length != 0) {
       for (let i of this.title) {
-        xlsData += `<th colspan=${i.colspan}>${i.name}`
+        xlsData += `<th colspan=${i.colspan}>${i.name}`;
       }
-      xlsData += '<th></tr>'
+      xlsData += "<th></tr>";
     }
     //Fields
     for (let key in data[0]) {
-      xlsData += '<th>' + key + '</th>'
+      xlsData += "<th>" + key + "</th>";
     }
-    xlsData += '</tr></thead>'
-    xlsData += '<tbody>'
+    xlsData += "</tr></thead>";
+    xlsData += "<tbody>";
     //Data
-    data.map(function (item, index) {
-      xlsData += '<tbody><tr>'
+    data.map(function(item, index) {
+      xlsData += "<tbody><tr>";
       for (let key in item) {
-        xlsData += '<td>' + item[key] + '</td>'
+        xlsData += "<td>" + item[key] + "</td>";
       }
-      xlsData += '</tr></tbody>'
-    })
+      xlsData += "</tr></tbody>";
+    });
     //Footer
     if (this.footer.length != 0) {
-      xlsData += '<tfooter><tr>'
+      xlsData += "<tfooter><tr>";
       for (let i of this.footer) {
-        xlsData += `<th colspan=${i.colspan}>${i.name}`
+        xlsData += `<th colspan=${i.colspan}>${i.name}`;
       }
-      xlsData += '<th></tr></tr></tfooter>'
+      xlsData += "<th></tr></tr></tfooter>";
     }
-    return xlsTemp.replace('${table}', xlsData)
+    return xlsTemp.replace("${table}", xlsData);
   }
   /*
   jsonToCSV
@@ -126,40 +136,40 @@ export default class Json2Excel {
   将json数据转换为CSV文件
   */
   jsonToCSV(data) {
-    var csvData = ''
+    var csvData = "";
     //Header
     if (this.title.length != 0) {
       for (let i of this.title) {
-        csvData += `${i.name}`
+        csvData += `${i.name}`;
       }
-      csvData += '\r\n'
+      csvData += "\r\n";
     }
     //Fields
     for (let key in data[0]) {
-      csvData += key + ','
+      csvData += key + ",";
     }
-    csvData = csvData.slice(0, csvData.length - 1)
-    csvData += '\r\n'
+    csvData = csvData.slice(0, csvData.length - 1);
+    csvData += "\r\n";
     //Data
-    data.map(function (item) {
+    data.map(function(item) {
       for (let key in item) {
-        let escapedCSV = item[key] + '' // cast Numbers to string
+        let escapedCSV = item[key] + ""; // cast Numbers to string
         if (escapedCSV.match(/[,"\n]/)) {
-          escapedCSV = '"' + escapedCSV.replace(/\"/g, '""') + '"'
+          escapedCSV = '"' + escapedCSV.replace(/\"/g, '""') + '"';
         }
-        csvData += escapedCSV + ','
+        csvData += escapedCSV + ",";
       }
-      csvData = csvData.slice(0, csvData.length - 1)
-      csvData += '\r\n'
-    })
+      csvData = csvData.slice(0, csvData.length - 1);
+      csvData += "\r\n";
+    });
     //Footer
     if (this.footer.length != 0) {
       for (let i of this.footer) {
-        csvData += `${i.name}`
+        csvData += `${i.name}`;
       }
-      csvData += '\r\n'
+      csvData += "\r\n";
     }
-    return csvData
+    return csvData;
   }
   /*
   getProcessedJson
@@ -167,31 +177,31 @@ export default class Json2Excel {
   仅获取要导出的数据，如果未设置任何字段则返回所有数据
   */
   getProcessedJson(data, header) {
-    let keys = this.getKeys(data, header)
-    let newData = []
-    let _self = this
-    data.map(function (item, index) {
-      let newItem = {}
+    let keys = this.getKeys(data, header);
+    let newData = [];
+    let _self = this;
+    data.map(function(item, index) {
+      let newItem = {};
       for (let label in keys) {
-        var iii = item
-        let property = keys[label]
-        newItem[label] = _self.getNestedData(property, item)
+        var iii = item;
+        let property = keys[label];
+        newItem[label] = _self.getNestedData(property, item);
       }
-      newData.push(newItem)
-    })
+      newData.push(newItem);
+    });
 
-    return newData
+    return newData;
   }
   getKeys(data, header) {
     if (header) {
-      return header
+      return header;
     }
 
-    let keys = {}
+    let keys = {};
     for (let key in data[0]) {
-      keys[key] = key
+      keys[key] = key;
     }
-    return keys
+    return keys;
   }
   /*
 parseExtraData
@@ -199,49 +209,52 @@ parseExtraData
 将标题和页脚属性解析为csv格式
 */
   parseExtraData(extraData, format) {
-    let parseData = ''
+    let parseData = "";
     if (Array.isArray(extraData)) {
       for (var i = 0; i < extraData.length; i++) {
-        parseData += format.replace('${data}', extraData[i])
+        parseData += format.replace("${data}", extraData[i]);
       }
     } else {
-      parseData += format.replace('${data}', extraData)
+      parseData += format.replace("${data}", extraData);
     }
-    return parseData
+    return parseData;
   }
   callItemCallback(field, itemValue) {
-    if (typeof field === 'object' && typeof field.callback === 'function') {
-      return field.callback(itemValue)
+    if (typeof field === "object" && typeof field.callback === "function") {
+      return field.callback(itemValue);
     }
-    return itemValue
+    return itemValue;
   }
   getNestedData(key, item) {
-    const field = typeof key === 'object' ? key.field : key
+    const field = typeof key === "object" ? key.field : key;
 
-    let valueFromNestedKey = null
-    let keyNestedSplit = field.split('.')
+    let valueFromNestedKey = null;
+    let keyNestedSplit = field.split(".");
 
-    valueFromNestedKey = item[keyNestedSplit[0]]
+    valueFromNestedKey = item[keyNestedSplit[0]];
     for (let j = 1; j < keyNestedSplit.length; j++) {
-      valueFromNestedKey = valueFromNestedKey[keyNestedSplit[j]]
+      valueFromNestedKey = valueFromNestedKey[keyNestedSplit[j]];
     }
 
-    valueFromNestedKey = this.callItemCallback(key, valueFromNestedKey)
+    valueFromNestedKey = this.callItemCallback(key, valueFromNestedKey);
 
-    valueFromNestedKey = valueFromNestedKey === null || valueFromNestedKey === undefined ? "" : valueFromNestedKey; // 过滤null、undefined的值
+    valueFromNestedKey =
+      valueFromNestedKey === null || valueFromNestedKey === undefined
+        ? ""
+        : valueFromNestedKey; // 过滤null、undefined的值
 
-    return valueFromNestedKey
+    return valueFromNestedKey;
   }
   base64ToBlob(data, mime) {
-    let base64 = window.btoa(window.unescape(encodeURIComponent(data)))
-    let bstr = atob(base64)
-    let n = bstr.length
-    let u8arr = new Uint8ClampedArray(n)
+    let base64 = window.btoa(window.unescape(encodeURIComponent(data)));
+    let bstr = atob(base64);
+    let n = bstr.length;
+    let u8arr = new Uint8ClampedArray(n);
     while (n--) {
-      u8arr[n] = bstr.charCodeAt(n)
+      u8arr[n] = bstr.charCodeAt(n);
     }
     return new Blob([u8arr], {
       type: mime
-    })
+    });
   }
 }
